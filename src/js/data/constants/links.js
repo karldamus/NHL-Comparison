c95bf32c31bfd2aa39f72393980fe7522cb8be5e
Karl Damus <getaholdofkarl@gmail.com>
2022-02-20 01:35:51 -0500

    Create links.js
    Useful API links to provide modularity and readability improvements -- see Changelog (0.1.0)

---
+++ src/js/data/constants/links.js
@@ -0,0 +1,44 @@
+let links = {
+    "root" : "https://statsapi.web.nhl.com/api/v1/",
+    "teams" : {
+        "root": "https://statsapi.web.nhl.com/api/v1/teams/",
+
+        "modifier" : {
+            "roster" : "?expand=team.roster",
+            "names" : "?expand=person.names",
+
+            "stats" : "?expand=team.stats",
+            "stats-from-season" : "?expand=team.roster&season=",
+
+            "scheduling" : {
+                "next" : "?expand=team.schedule.next",
+                "prev" : "?expand=team.schedule.previous"
+            }
+        }
+    },
+    
+    "people" : {
+        "root" : "https://statsapi.web.nhl.com/api/v1/people/",
+
+        "modifier" : {
+
+        }
+    },
+
+    "game" : {
+        "root" : "https://statsapi.web.nhl.com/api/v1/game/",
+
+        "live-feed" : "feed/live",
+        "box-score" : "boxscore",
+        "line-score" : "linescore",
+        "content" : "content"
+    },
+
+    "divisions" : {
+        "root" : "https://statsapi.web.nhl.com/api/v1/divisions/"
+    },
+
+    "conferences" : {
+        "root" : "https://statsapi.web.nhl.com/api/v1/conferences/"
+    }
+}
