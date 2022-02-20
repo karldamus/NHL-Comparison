654d95f2c744dfea4cfebc910ff12174e9f37728
Karl Damus <getaholdofkarl@gmail.com>
2022-02-20 01:35:19 -0500

    Create filePaths.js
    File paths for all js files -- see ScriptLoader

---
+++ src/js/data/constants/filePaths.js
@@ -0,0 +1,16 @@
+// general js and json
+let helpers = "js/helpers/";
+let constants = "js/data/constants/";
+
+// data specific js
+let games = "js/data/games/";
+let players = "js/data/players/";
+let teams = "js/data/teams/";
+let tournaments = "js/data/tournaments/";
+
+let filePaths = [
+    constants + "links.js",
+    helpers + "getData.js",
+    teams + "singleTeam.js",
+    "js/main.js"
+]
