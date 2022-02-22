643dcead0a01b6e451e7bcb3810d760eb17700e9
Karl Damus <getaholdofkarl@gmail.com>
2022-02-22 00:15:00 -0500

    Update getData.js
    Added URL checking -- not working - TODO: fix

--- src/js/helpers/getData.js
+++ src/js/helpers/getData.js
@@ -4,6 +4,18 @@
  * @returns 
  */
  async function getData(URL) {
-    const response = await fetch(URL);
-    return response.json();
+     if (URLExists(URL)) {
+        const response = await fetch(URL);
+        return response.json();
+     } else {
+         console.error("URL returned 404 for GET request on URL: " + URL);
+     }
+}
+
+function URLExists(URL) {
+    var http = new XMLHttpRequest();
+    http.open('GET', URL, false);
+    http.send();
+
+    return (http.status != 404);
 }
