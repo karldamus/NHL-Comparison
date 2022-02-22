f4b470006d8570854856b1ffc695dcaf03f0f091
Karl Damus <getaholdofkarl@gmail.com>
2022-02-22 00:14:33 -0500

    Update singleTeam.js
    Added ability to get general stats for a team
    - see main.js

--- src/js/data/teams/singleTeam.js
+++ src/js/data/teams/singleTeam.js
@@ -1,39 +1,109 @@
 let basicTeamData = {};
 
 async function displayAllTeamInfo() {
-    for (let [key, value] of Object.entries(basicTeamData)) {
-        // getTeamInfo(value.teamId);
-        // console.log(value);
-        // getTeamStats(value.teamId);
-    }
+    // for (let [key, value] of Object.entries(basicTeamData)) {
+    //     // getTeamInfo(value.teamId);
+    //     // console.log(value);
+    //     // getTeamStats(value.teamId);
+    //     getTeamRoster(10);
+    // }
 }
 
+/**
+ * Basic team information for the team with id teamId -> includes:
+ *      name; site; location/venue;
+ *      franchise, conference, and division information
+ * @param {*} teamId 
+ */
 async function getTeamInfo(teamId) {
-    if (!Number.isInteger(teamId)) {
-        // convert team name to id
-        teamId = await getTeamIdFromName(teamId);
-    }
+    teamId = await validateTeamId(teamId);
 
     let URL = links.teams.root + teamId;
     var data = await getData(URL);
 
-    for (let [key, value] of Object.entries(data.teams)) {
-        console.log(value);
-    }
+    // for (let [key, value] of Object.entries(data.teams)) {
+    //     // console.log(value);
+    // }
+
+    return data.teams;
 }
 
-async function getTeamStats(teamId) {
-    if (!Number.isInteger(teamId)) {
-        // convert team name to id
-        teamId = await getTeamIdFromName(teamId);
+async function returnValuableTeamInfo(teamId) {
+    let teamData = [];
+
+    teamId = await validateTeamId(teamId);
+
+    var tmpTeamInfo = (await getTeamInfo(teamId))[0];
+    var tmpTeamStats = (await getTeamStats(teamId)).stat;
+    var tmpTeamRoster = (await getTeamRoster(teamId)).roster;
+
+    let teamInfo = {};
+    let teamStats = {};
+    let teamRoster = {};
+
+    // teamInfo
+    teamInfo.teamName = tmpTeamInfo.teamName;
+    teamInfo.conference = tmpTeamInfo.conference.name;
+    teamInfo.division = tmpTeamInfo.division.name;
+
+    // teamStats
+    teamStats = tmpTeamStats;
+
+    // teamRoster
+    for (let [key, value] of Object.entries(tmpTeamRoster)) {
+        let player = {};
+
+        player.fullName = value.person.fullName;
+        player.jerseyNumber = value.jerseyNumber;
+        player.link = links.root + value.person.link;
+
+        teamRoster[key] = player;
     }
-    
+
+    teamData[0] = teamInfo;
+    teamData[1] = teamStats;
+    teamData[2] = teamRoster;
+
+    console.log(tmpTeamInfo);
+    console.log(tmpTeamStats);
+    console.log(tmpTeamRoster);
+
+    console.log(teamData);
+
+    return teamData;
+}
+
+/**
+ * Current seasons' stats for the team with id teamId
+ * @param {*} teamId 
+ */
+async function getTeamStats(teamId) {
+    teamId = await validateTeamId(teamId);
+
     let URL = links.teams.root + teamId + links.teams.modifier.stats;
     var data = await getData(URL);
 
-    console.log(data.teams[0].teamName);
-    console.log(data.teams[0].teamStats[0].splits[0]);
+    // console.log(data.teams[0].teamName);
+    // console.log(data.teams[0].teamStats[0].splits[0]);
+
+    return data.teams[0].teamStats[0].splits[0];
 }
+
+/**
+ * Current roster for the team with id teamId
+ * @param {*} teamId 
+ */
+async function getTeamRoster(teamId) {
+    teamId = await validateTeamId(teamId);
+
+    let URL = links.teams.root + teamId + links.teams.modifier.roster;
+    var data = await getData(URL);
+
+    // console.log(data.teams[0].roster);
+
+    return data.teams[0].roster;
+}
+
 
 //
 // setup basic data
@@ -47,7 +117,7 @@ async function getTeamStats(teamId) {
     let URL = links.teams.root;
     var data = await getData(URL);
 
-    console.log(data);
+    // console.log(data);
 
     for (let [key, value] of Object.entries(data.teams)) {
         // if (value.)
@@ -58,16 +128,17 @@ async function getTeamStats(teamId) {
             locationName: value.locationName,
             teamName: value.teamName
         };
-
-        // console.log(basicTeamData[key]);
     }
 }
 
-async function getTeamIdFromName(teamName) {
+/**
+ * Get the id of a team from the name:
+ * @param {*} teamName the name of the team in short-form, long-form, or just the location name
+ * @returns 
+ */
+ async function getTeamIdFromName(teamName) {
     let URL = links.teams.root;
     var data = await getData(URL);
-
-    console.log(teamName);
 
     for (let [key, value] of Object.entries(data.teams)) {
         let tmpLocationName = value.locationName.toLowerCase();
@@ -79,5 +150,17 @@ async function getTeamIdFromName(teamName) {
         }
     }
 
+    console.error("Could not find the id of team with input: " + teamName);
+
     return -1;
+}
+
+function getTeamNameFromId(teamId) {
+    for (let [key, value] of Object.entries(basicTeamData)) {
+        if (value.teamId == teamId) {
+            return value.fullName;
+        }
+    }
+
+    return "No team found";
 }
